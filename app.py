89edd3fa6c692750690848b462841f16c43b90b6
Naresh_Ram <43650120+nareshram256@users.noreply.github.com>
2022-06-21 23:25:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -129,12 +129,11 @@ if len(files)>0:
         else:
           f.append(0)
           A.append(0)
-          s.append(df2.["time"][int(sample)*(k+1)]
+          s.append(df2["time"][int(sample)*(k+1)]
       f=np.asarray(f)
       A=np.asarray(A)
       ff.append(f)
       AA.append(A)
-      s=np.asarray(s)
       fig.add_trace(go.Scatter(x=s, y=A,
                             mode='lines+text',yaxis='y1',
                             text=f,
