0babb2dd521ecfc1bf4da7400a188bc98d767380
Naresh_Ram <43650120+nareshram256@users.noreply.github.com>
2022-06-22 16:53:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ else:
     c=[1,0.4,0.2]
 #st.sidebar.write('a=',list(a))
 #st.sidebar.write(list(b))
+check=0
 Y=[]
 q=[]
 datetime_object = datetime.now()
@@ -67,17 +68,20 @@ st.sidebar.download_button(
 dest="oscfiles/"
 dest1="databse/"
 June="old/"
+
 import pandas as pd
 from glob import glob
-#if st.button('clear the data'):
-#    files = sorted(glob(dest+'*.xlsx'))
-#    if len(files)>0:
-#        for fil in files:
-#            os.remove(fil)
+if st.button('clear the data'):
+    files = sorted(glob(dest+'*.xlsx'))
+    if len(files)>0:
+        for fil in files:
+            os.remove(fil)
 import warnings
 warnings.simplefilter("ignore")
 
-if st.button('clear June data and upload custom'):
+
+check=st.number_input('1: default/June data    \t    2: Custom Upload')
+if(check==2):   
     spectras = st.file_uploader("upload file", type={"xlsx"},accept_multiple_files = True)
     for spectra in spectras:
         if spectra is not None:
@@ -87,179 +91,176 @@ if st.button('clear June data and upload custom'):
                 f.write((spectra).getbuffer())    
         else:
             st.write("Upload excel files")
+
     files = sorted(glob(dest+'*.xlsx'))
-
-else:
+elif (check==1):   
     files = sorted(glob(June+'*.xlsx'))
+
     
-
-
-
-        
-
 #st.write('You selected:', option)
 
 #st.write('you selected',np.argwhere(ll==option)[0])
 
 Volt=[]
-na=[]    
-if(len(files)>0):
-    Data=pd.read_excel(files[0],engine='openpyxl')
-    ll=np.asarray(Data.columns[1:])
-    option = st.selectbox(
-    'Which Oscillations plot you like?',
-     ll)
-    for fil in files:
-        Data=pd.read_excel(fil,engine='openpyxl')
-        try:
-            Volt.append(Data.iloc[:,int(np.argwhere(ll==option))+1])
-            na.append(Data.iloc[0][1])
-        except:
-            continue
+na=[]
+try:
+    if(len(files)>0):
+        Data=pd.read_excel(files[0],engine='openpyxl')
+        ll=np.asarray(Data.columns[1:])
+        option = st.selectbox(
+        'Which Oscillations plot you like?',
+         ll)
+        for fil in files:
+            Data=pd.read_excel(fil,engine='openpyxl')
+            try:
+                Volt.append(Data.iloc[:,int(np.argwhere(ll==option))+1])
+                na.append(Data.iloc[0][1])
+            except:
+                continue
 
-    import numpy as np
-    #st.write(len(Volt))
-    df2 = pd.DataFrame()
-    df2["time"] = pd.to_datetime(Data[Data.columns[0]])
-    
-    import plotly.graph_objects as go
-    fig = go.Figure()
-    for r in range (0,len(Volt)):
-        df2[na[r]] = Volt[r]/np.max(Volt[r])
-        #st.write(Volt[r],np.max(Volt[r]))
-        fig.add_trace(go.Scatter(x=df2["time"], y=df2[na[r]],
-                          mode='lines',
-                          name=str(na[r])))
+        import numpy as np
+        #st.write(len(Volt))
+        df2 = pd.DataFrame()
+        df2["time"] = pd.to_datetime(Data[Data.columns[0]])
+
+        import plotly.graph_objects as go
+        fig = go.Figure()
+        for r in range (0,len(Volt)):
+            df2[na[r]] = Volt[r]/np.max(Volt[r])
+            #st.write(Volt[r],np.max(Volt[r]))
+            fig.add_trace(go.Scatter(x=df2["time"], y=df2[na[r]],
+                              mode='lines',
+                              name=str(na[r])))
 
 
+            fig.update_layout(
+            autosize=True,
+            yaxis=dict(
+                title_text="Amplitude (PU)",
+                titlefont=dict(size=30),),
+            #yaxis2=dict(title='Freq',overlaying='y',side='right',titlefont=dict(size=30),),
+            xaxis=dict(
+                title_text="Time",
+                titlefont=dict(size=30),
+
+            ),
+          )
+        fig.update_yaxes(automargin=True)
+        st.header(option+" plot")
+        st.plotly_chart(fig)
+        #fig.show()
+        st.header(" Description of data ")
+        st.write(df2.describe())
+        df2=df2.fillna(0)
+        #df2.to_csv(dest+"data2.csv", index=False)
+        st.download_button(
+             label="Download data as CSV",
+             data=df2.to_csv().encode('utf-8'),
+             file_name='data.csv',
+             mime='text/csv',
+         )
+
+        import matplotlib.pyplot as plt
+        import scipy.fft
+
+        fig = go.Figure()
+        ff=[]
+        AA=[]
+        sample= st.select_slider(
+         'Select sample length',
+         options=[100, 200, 400, 800, 1000, 1200, 1400])
+        st.write('chosen sample length is', sample)
+        #st.write("max frequency detected would be %f Hz"%(25/options))
+        for r in range (1,df2.shape[1]):
+          f=[]
+          A=[]
+          s=[]
+          for k in range (int(df2.shape[0]/int(sample))-1):
+            data1=df2.iloc[int(sample)*k:int(sample)*(k+1),r].values
+            A_signal_fft = scipy.fft.fft(data1)
+            #A_signal_fft=np.sqrt(A_signal_fft.real**2+A_signal_fft.imag**2)
+            frequencies = scipy.fft.fftfreq(int(sample), 1/(25))
+            df3 = pd.DataFrame()
+            df3['freq']=np.abs(frequencies[1:])
+            df3['amp']=np.abs(A_signal_fft)[1:]/int(sample)
+            #st.write(df3[(df3['freq']>1/(int(sample)*0.04)) & (df3['freq']<1)]['amp'].max())
+            dummy=df3[(df3['freq']>1/(int(sample)*0.04)) & (df3['freq']<1)]['amp'].max()
+            #if(len(df3[df3['amp']==df3['amp'].max()]['freq'].values)>0 and df3[df3['amp']==df3['amp'].max()]['freq'].values[0] >1/(int(sample)*0.04) and df3[df3['amp']==df3['amp'].max()]['freq'].values[0]<1):
+            if(len(df3[df3['amp']==dummy]['freq'].values)>0):
+              f.append(np.round(df3[df3['amp']==dummy]['freq'].values[0],2)) 
+              A.append(dummy)
+              #st.write(int(sample)*(k+1),df2["time"])
+              s.append(df2.iloc[int(sample)*(k+1),0])
+            else:
+              #st.write(df2.columns[r])
+              #st.write(df3[df3['amp']==df3['amp'].max()]['freq'].values)
+              f.append(0)
+              A.append(0)
+              s.append(df2.iloc[int(sample)*(k+1),0])
+          f=np.asarray(f)
+          A=np.asarray(A)
+          ff.append(f)
+          AA.append(A)
+          fig.add_trace(go.Scatter(x=s, y=A,
+                                mode='lines+text',yaxis='y1',
+                                text=f,
+                                name=str(df2.columns[r])))
+          #fig.add_trace(go.Scatter(x=s, y=f,mode='markers',yaxis='y2',))
         fig.update_layout(
-        autosize=True,
-        yaxis=dict(
-            title_text="Amplitude (PU)",
-            titlefont=dict(size=30),),
-        #yaxis2=dict(title='Freq',overlaying='y',side='right',titlefont=dict(size=30),),
-        xaxis=dict(
-            title_text="Time",
-            titlefont=dict(size=30),
+            autosize=True,
+            #width=1500,
+            #height=800,
+            yaxis=dict(
+                title_text="Amplitude (PU)",
+                titlefont=dict(size=30),),
+            #yaxis2=dict(title='Freq',overlaying='y',side='right',titlefont=dict(size=30),),
+            xaxis=dict(
+                title_text="Time ",
+                titlefont=dict(size=30),
 
-        ),
-      )
-    fig.update_yaxes(automargin=True)
-    st.header(option+" plot")
-    st.plotly_chart(fig)
-    #fig.show()
-    st.header(" Description of data ")
-    st.write(df2.describe())
-    df2=df2.fillna(0)
-    df2.to_csv(dest+"data2.csv", index=False)
-    st.download_button(
-         label="Download data as CSV",
-         data=df2.to_csv().encode('utf-8'),
-         file_name='data.csv',
-         mime='text/csv',
-     )
-
-    import matplotlib.pyplot as plt
-    import scipy.fft
-
-    fig = go.Figure()
-    ff=[]
-    AA=[]
-    sample= st.select_slider(
-     'Select sample length',
-     options=[100, 200, 400, 800, 1000, 1200, 1400])
-    st.write('chosen sample length is', sample)
-    #st.write("max frequency detected would be %f Hz"%(25/options))
-    for r in range (1,df2.shape[1]):
-      f=[]
-      A=[]
-      s=[]
-      for k in range (int(df2.shape[0]/int(sample))-1):
-        data1=df2.iloc[int(sample)*k:int(sample)*(k+1),r].values
-        A_signal_fft = scipy.fft.fft(data1)
-        #A_signal_fft=np.sqrt(A_signal_fft.real**2+A_signal_fft.imag**2)
-        frequencies = scipy.fft.fftfreq(int(sample), 1/(25))
-        df3 = pd.DataFrame()
-        df3['freq']=np.abs(frequencies[1:])
-        df3['amp']=np.abs(A_signal_fft)[1:]/int(sample)
-        #st.write(df3[(df3['freq']>1/(int(sample)*0.04)) & (df3['freq']<1)]['amp'].max())
-        dummy=df3[(df3['freq']>1/(int(sample)*0.04)) & (df3['freq']<1)]['amp'].max()
-        #if(len(df3[df3['amp']==df3['amp'].max()]['freq'].values)>0 and df3[df3['amp']==df3['amp'].max()]['freq'].values[0] >1/(int(sample)*0.04) and df3[df3['amp']==df3['amp'].max()]['freq'].values[0]<1):
-        if(len(df3[df3['amp']==dummy]['freq'].values)>0):
-          f.append(np.round(df3[df3['amp']==dummy]['freq'].values[0],2)) 
-          A.append(dummy)
-          #st.write(int(sample)*(k+1),df2["time"])
-          s.append(df2.iloc[int(sample)*(k+1),0])
-        else:
-          #st.write(df2.columns[r])
-          #st.write(df3[df3['amp']==df3['amp'].max()]['freq'].values)
-          f.append(0)
-          A.append(0)
-          s.append(df2.iloc[int(sample)*(k+1),0])
-      f=np.asarray(f)
-      A=np.asarray(A)
-      ff.append(f)
-      AA.append(A)
-      fig.add_trace(go.Scatter(x=s, y=A,
-                            mode='lines+text',yaxis='y1',
-                            text=f,
-                            name=str(df2.columns[r])))
-      #fig.add_trace(go.Scatter(x=s, y=f,mode='markers',yaxis='y2',))
-    fig.update_layout(
-        autosize=True,
-        #width=1500,
-        #height=800,
-        yaxis=dict(
-            title_text="Amplitude (PU)",
-            titlefont=dict(size=30),),
-        #yaxis2=dict(title='Freq',overlaying='y',side='right',titlefont=dict(size=30),),
-        xaxis=dict(
-            title_text="Time ",
-            titlefont=dict(size=30),
-
-        ),
-      )
+            ),
+          )
 
 
-    fig.update_yaxes(automargin=True)
-    #fig.show()
-    st.header("Frequency-Time")
-    st.plotly_chart(fig)
+        fig.update_yaxes(automargin=True)
+        #fig.show()
+        st.header("Frequency-Time")
+        st.plotly_chart(fig)
 
-    df4=pd.DataFrame()
-    ff=np.asarray(ff)
-    AA=np.asarray(AA)
-    fig = go.Figure()
+        df4=pd.DataFrame()
+        ff=np.asarray(ff)
+        AA=np.asarray(AA)
+        fig = go.Figure()
 
-    for k in range (int(0.5*df2.shape[0]/int(sample))):
-      df4['freq']=ff[:,k*2]
-      df4['amp']=AA[:,k*2]
-
-
-      fig.add_trace(go.Scatter(x=df2.columns[1:], y=df4['amp'],
-                              mode='lines+text',yaxis='y1',
-                              text=ff[:,k*2],
-                              name='sample' +str(k*2)))
-        #fig.add_trace(go.Scatter(x=s, y=f,mode='markers',yaxis='y2',))
-    fig.update_layout(
-        autosize=True,
-        #width=1500,
-        #height=800,
-        yaxis=dict(
-            title_text="Amplitude (PU)",
-            titlefont=dict(size=30),),
-        #yaxis2=dict(title='Freq',overlaying='y',side='right',titlefont=dict(size=30),),
-        xaxis=dict(
-            title_text="stations",
-            titlefont=dict(size=30),
-
-        ),
-      )
+        for k in range (int(0.5*df2.shape[0]/int(sample))):
+          df4['freq']=ff[:,k*2]
+          df4['amp']=AA[:,k*2]
 
 
-    fig.update_yaxes(automargin=True)
-    st.header("plant wise plot")
-    st.plotly_chart(fig)
-   
-    
-    st.sidebar.text("Developped by NR LDC")
+          fig.add_trace(go.Scatter(x=df2.columns[1:], y=df4['amp'],
+                                  mode='lines+text',yaxis='y1',
+                                  text=ff[:,k*2],
+                                  name='sample' +str(k*2)))
+            #fig.add_trace(go.Scatter(x=s, y=f,mode='markers',yaxis='y2',))
+        fig.update_layout(
+            autosize=True,
+            #width=1500,
+            #height=800,
+            yaxis=dict(
+                title_text="Amplitude (PU)",
+                titlefont=dict(size=30),),
+            #yaxis2=dict(title='Freq',overlaying='y',side='right',titlefont=dict(size=30),),
+            xaxis=dict(
+                title_text="stations",
+                titlefont=dict(size=30),
+
+            ),
+          )
+
+
+        fig.update_yaxes(automargin=True)
+        st.header("plant wise plot")
+        st.plotly_chart(fig)
+        
+except:
+    pass
